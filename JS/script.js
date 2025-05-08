87bfaee2e86ec4a6d7d9a37f69746227c5da5dd4
Cruxzit <hugocruxzit@gmail.com>
2025-05-08 21:05:24 +0100

    lista das palavras e variaveis para as funcoes

---
+++ JS/script.js
@@ -0,0 +1,18 @@
+//lista das palavras 
+
+const palavras = ['MAÇA', 'BANANA', 'LARANJA', 'KIWI', 'PERA', 'ANANAS', 'MANGA', 'MELANCIA', 'MELAO', 'MORANGO', 'UVA', 'CEREJA', 'PESSEGO',  'TANGERINA', 'LIMAO', 'COCO', 'FIGO'];
+
+
+//variavel para selecionar a palavra
+let palavra_selecionada = '';
+
+//variavel para as letras adivinhadas
+let letras_adivinhadas = [];
+
+//variavel para o numero de tentativas erradas
+let tentativas_erradas = 0;
+
+//variavel para ver se o jogo ja acabou
+let game_over = false;
+
+
