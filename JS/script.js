727d45608ee43b47940865a02b62072a739642c1
Cruxzit <hugocruxzit@gmail.com>
2025-05-08 23:11:26 +0100

    elementos do DOM

--- JS/script.js
+++ JS/script.js
@@ -16,3 +16,12 @@ let tentativas_erradas = 0;
 let game_over = false;
 
 
+//elementos do DOM
+const palavra_escondida_el = document.getElementById('palavra_escondida');
+const tentativas_restantes_el = document.getElementById('tentativas_restantes');
+const mensagem_el = document.getElementById('mensagem');
+const teclado_el = document.getElementById('teclado');
+const botao_reiniciar = document.getElementById('botao_reiniciar');
+const letras_corretas_el = document.getElementById('letras_corretas');
+const letras_restantes_el = document.getElementById('letras_restantes');
+const total_letras_el = document.getElementById('total_letras');
